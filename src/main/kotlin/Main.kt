6b0636e6e2234b880b94f954ac72b3a253863add
MutindaVictoria <victoriaamutinda@gmail.com>
2023-03-09 08:59:13 +0300

    first commit

---
+++ src/main/kotlin/Main.kt
@@ -0,0 +1,31 @@
+fun main(){
+    numbers()
+     name()
+strings()
+
+}
+
+//Write and invoke a function that takes in any 2 strings and returns the first
+//character of the longer string or the first character of the first string if they are
+//equal in length.
+fun strings(){
+    var b="numbers"
+    var c= "car"
+
+    return strings()
+}
+
+//Write and invoke one function that takes in an array of integers and returns
+//these 3 values: smallest, largest and average of all the elements
+fun numbers(){
+    var numbers = arrayOf(12, 1, 3)
+    (numbers.sortedArrayDescending())
+    println(numbers.sortedArray())
+return numbers()
+}
+//Write and call a function that takes in a string and splits it into all the
+//characters that constitute it. Your function should print out this output.
+fun name(){
+    var x= "my is carol"
+    println(x.toString())
+}
